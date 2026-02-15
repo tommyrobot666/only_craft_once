4a7ea9c2e66fa67a8f8c2fd8a38284a497cce8a5
tommyrobot666 <tommyrobot10@gmail.com>
2026-02-15 03:43:36 -0800

    found KnotClassLoader, but didn't know what to do after

---
+++ fabric/src/main/java/lommie/only_craft_once/PreLaunch.java
@@ -0,0 +1,20 @@
+package lommie.only_craft_once;
+
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.fabricmc.loader.impl.launch.FabricLauncherBase;
+
+public class PreLaunch implements PreLaunchEntrypoint {
+    @Override
+    public void onPreLaunch() {
+        try {
+            ClassLoader classLoader = FabricLauncherBase.getLauncher().getTargetClassLoader();
+            Class<?> knotLoader = Class.forName("net.fabricmc.loader.impl.launch.knot.KnotClassLoader", false, classLoader);
+            if (knotLoader.isInstance(classLoader)){
+                System.out.println("found KnotClassLoader");
+            }
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
