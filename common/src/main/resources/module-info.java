1264fe9e5fb619ec6f9be8e6899a4b223611d1a1
tommyrobot666 <tommyrobot10@gmail.com>
2026-02-15 17:25:36 -0800

    clean up failed attempts

--- common/src/main/resources/module-info.java
+++
@@ -1,7 +0,0 @@
-// file only works when put in src/main/java
-//module lommie.only_craft_once {
-// makes it only have org.objectweb.asm
-//    requires org.objectweb.asm;
-
-//    requires vanilla;
-//}
