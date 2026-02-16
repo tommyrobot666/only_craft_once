7d9e68e2b49823d976507b5ec675a37c690e828e
tommyrobot666 <tommyrobot10@gmail.com>
2026-02-15 17:14:59 -0800

    something finally worked. disabled crafting

---
+++ common/src/main/java/lommie/only_craft_once/mixin/RecipeManagerMixin.java
@@ -0,0 +1,32 @@
+package lommie.only_craft_once.mixin;
+
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
+
+@Mixin(RecipeManager.class)
+public class RecipeManagerMixin {
+//    @Inject(
+//            method = "*",
+//            at = @At(
+//                    value = "INVOKE",
+//                    target = "Lnet/minecraft/world/item/crafting/Recipe;matches(Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/world/level/Level;)Z"
+//                )
+//    )
+    @Inject(
+            method = "getRecipeFor(Lnet/minecraft/world/item/crafting/RecipeType;Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/crafting/RecipeHolder;)Ljava/util/Optional;",
+            at = @At("HEAD"),
+            cancellable = true)
+    <I extends RecipeInput, T extends Recipe<@NotNull I>>
+    void checkIfCraftedBefore(RecipeType<@NotNull T> recipeType, I input, Level level, @Nullable RecipeHolder<@NotNull T> lastRecipe, CallbackInfoReturnable<Optional<RecipeHolder<@NotNull T>>> cir){
+        cir.setReturnValue(Optional.empty());
+        cir.cancel();
+    }
+}
