6f1b7855c30587016b035ccb0a85e5f0fa4f87ec
tommyrobot666 <tommyrobot10@gmail.com>
2026-02-15 23:10:42 -0800

    do checkIfCraftedBefore for all getRecipeFor calls

--- common/src/main/java/lommie/onlycraftonce/mixin/RecipeManagerMixin.java
+++ common/src/main/java/lommie/onlycraftonce/mixin/RecipeManagerMixin.java
@@ -3,6 +3,7 @@ package lommie.onlycraftonce.mixin;
 import lommie.onlycraftonce.CommonClass;
 import lommie.onlycraftonce.Constants;
 import lommie.onlycraftonce.saveddata.TimesCraftedSavedData;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -11,6 +12,7 @@ import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,6 +21,8 @@ import java.util.Optional;
 
 @Mixin(RecipeManager.class)
 public class RecipeManagerMixin {
+    @Shadow private RecipeMap recipes;
+
     @Inject(
             method = "getRecipeFor(Lnet/minecraft/world/item/crafting/RecipeType;Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/crafting/RecipeHolder;)Ljava/util/Optional;",
             at = @At("HEAD"),
@@ -41,5 +45,30 @@ public class RecipeManagerMixin {
                 }
             }
         }
+    }
+
+    @Inject(
+            method = "getRecipeFor(Lnet/minecraft/world/item/crafting/RecipeType;Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/world/level/Level;Lnet/minecraft/resources/ResourceKey;)Ljava/util/Optional;",
+            at = @At("HEAD"),
+            cancellable = true)
+    <I extends RecipeInput, T extends Recipe<@NotNull I>>
+    void checkIfCraftedBefore2(RecipeType<T> recipeType, I input, Level level, @Nullable ResourceKey<Recipe<?>> recipe, CallbackInfoReturnable<Optional<RecipeHolder<T>>> cir){
+//        checkIfCraftedBefore(recipeType,input,level,recipe==null?null:new RecipeHolder<>(recipe, (T) level.registryAccess().getOrThrow(recipe).value()),cir);
+        checkIfCraftedBefore2(recipeType,input,level,cir);
+    }
+
+    @Inject(
+            method = "getRecipeFor(Lnet/minecraft/world/item/crafting/RecipeType;Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/world/level/Level;)Ljava/util/Optional;",
+            at = @At("HEAD"),
+            cancellable = true)
+    <I extends RecipeInput, T extends Recipe<@NotNull I>>
+    void checkIfCraftedBefore2(RecipeType<T> recipeType, I input, Level level, CallbackInfoReturnable<Optional<RecipeHolder<T>>> cir){
+        Optional<RecipeHolder<T>> recipeOpt = this.recipes.getRecipesFor(recipeType,input,level).findFirst();
+        if (recipeOpt.isEmpty()){
+            cir.setReturnValue(Optional.empty());
+            cir.cancel();
+        }
+
+        checkIfCraftedBefore(recipeType,input,level,recipeOpt.orElseThrow(),cir);
     }
 }
