f089731ed2c3537f6313c01d7bbfce2fd76fd5da
tommyrobot666 <tommyrobot10@gmail.com>
2026-06-30 21:23:12 -0700

    rewrite RecipeManagerMixin to just check return value

--- common/src/main/java/lommie/onlycraftonce/mixin/RecipeManagerMixin.java
+++ common/src/main/java/lommie/onlycraftonce/mixin/RecipeManagerMixin.java
@@ -20,53 +20,39 @@ import java.util.Optional;
 
 @Mixin(RecipeManager.class)
 public abstract class RecipeManagerMixin {
-    @Shadow private RecipeMap recipes;
-
-    @Shadow @Nullable protected abstract <T extends Recipe<?>> RecipeHolder<T> byKeyTyped(RecipeType<T> type, ResourceKey<Recipe<?>> key);
-
     @Inject(
-            method = "getRecipeFor(Lnet/minecraft/world/item/crafting/RecipeType;Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/crafting/RecipeHolder;)Ljava/util/Optional;",
-            at = @At("HEAD"),
+            method = "getRecipeFor(Lnet/minecraft/world/item/crafting/RecipeType;Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/world/level/Level;)Ljava/util/Optional;",
+            at = @At("RETURN"),
             cancellable = true)
     <I extends RecipeInput, T extends Recipe<@NotNull I>>
-    void checkIfCraftedBefore(RecipeType<@NotNull T> recipeType, I input, Level level, @Nullable RecipeHolder<@NotNull T> lastRecipe, CallbackInfoReturnable<Optional<RecipeHolder<@NotNull T>>> cir){
-        if (lastRecipe == null || level.isClientSide()) return;
-        ItemStack result = lastRecipe.value().assemble(input,level.registryAccess());
+    void checkIfCraftedBefore(RecipeType<T> recipeType, I input, Level level, CallbackInfoReturnable<Optional<RecipeHolder<T>>> cir){
+        if (cir.getReturnValue().isEmpty()) return;
+        var recipeHolder = cir.getReturnValue().get();
+        ItemStack result = recipeHolder.value().assemble(input,level.registryAccess());
+        Item item = result.getItem();
         TimesCraftedSavedData savedData = TimesCraftedSavedData.get(((ServerLevel) level));
-        for (Item item : CommonClass.maxTimesCrafted.keySet()) {
-            if (result.is(item)){
-                if (result.getCount() + savedData.map.getOrDefault("minecraft.crafted:"+item.toString().replace(':','.'),0) > CommonClass.maxTimesCrafted.get(item)){
-                    cir.setReturnValue(Optional.empty());
-                    cir.cancel();
-                    return;
-                }
-            }
+        String key = "minecraft.crafted:"+item.toString().replace(':','.');
+        if (!savedData.map.containsKey(key)) return;
+        if (savedData.map.get(key) + result.getCount() > CommonClass.maxTimesCrafted.get(item)) {
+            cir.setReturnValue(Optional.empty());
         }
     }
 
     @Inject(
             method = "getRecipeFor(Lnet/minecraft/world/item/crafting/RecipeType;Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/world/level/Level;Lnet/minecraft/resources/ResourceKey;)Ljava/util/Optional;",
-            at = @At("HEAD"),
+            at = @At("RETURN"),
             cancellable = true)
     <I extends RecipeInput, T extends Recipe<@NotNull I>>
     void checkIfCraftedBefore2(RecipeType<T> recipeType, I input, Level level, @Nullable ResourceKey<Recipe<?>> recipe, CallbackInfoReturnable<Optional<RecipeHolder<T>>> cir){
-        RecipeHolder<T> holder = recipe!=null?this.byKeyTyped(recipeType,recipe):null;
-        checkIfCraftedBefore(recipeType,input,level,holder,cir);
+        checkIfCraftedBefore(recipeType,input,level,cir);
     }
 
     @Inject(
-            method = "getRecipeFor(Lnet/minecraft/world/item/crafting/RecipeType;Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/world/level/Level;)Ljava/util/Optional;",
-            at = @At("HEAD"),
+            method = "getRecipeFor(Lnet/minecraft/world/item/crafting/RecipeType;Lnet/minecraft/world/item/crafting/RecipeInput;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/crafting/RecipeHolder;)Ljava/util/Optional;",
+            at = @At("RETURN"),
             cancellable = true)
     <I extends RecipeInput, T extends Recipe<@NotNull I>>
-    void checkIfCraftedBefore2(RecipeType<T> recipeType, I input, Level level, CallbackInfoReturnable<Optional<RecipeHolder<T>>> cir){
-        Optional<RecipeHolder<T>> recipeOpt = this.recipes.getRecipesFor(recipeType,input,level).findFirst();
-        if (recipeOpt.isEmpty()){
-            cir.setReturnValue(Optional.empty());
-            cir.cancel();
-            return;
-        }
-
-        checkIfCraftedBefore(recipeType,input,level,recipeOpt.orElseThrow(),cir);
+    void checkIfCraftedBefore2(RecipeType<T> recipeType, I input, Level level, @Nullable RecipeHolder<T> lastRecipe, CallbackInfoReturnable<Optional<RecipeHolder<T>>> cir){
+        checkIfCraftedBefore(recipeType,input,level,cir);
     }
 }
